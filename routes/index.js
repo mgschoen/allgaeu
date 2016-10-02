715c44fdf27a21b2950864d9252156bed5511cdb
Martin Schoen <schoenm@cip.ifi.lmu.de>
2016-10-02 23:52:56 +0200

    Added connection to mongo collection sessions.hashes in index route

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  var db = req.db;
+  var hashCollection = db.get('sessions.hashes');
+  var hashToInsert = {
+    hash: "woieusdf", // TODO replace with actual hash
+    creationDate: new Date()
+  };
+  hashCollection.insert(hashToInsert, {}, function(err,result){
+    if (err === null) {
+      res.render('index', {
+        title: 'Express',
+        authHash: hashToInsert.hash
+      });
+    } else {
+      console.log(err);
+      return false;
+    }
+  });
 });
 
 /* GET JSON representation of 'content' collection in db */
 router.get('/content', function(req, res){
   var db = req.db;
-  var collection = db.get('content');
-  collection.find({}, "-_id", function(e, docs){
+  var contentCollection = db.get('content');
+  contentCollection.find({}, "-_id", function(e, docs){
     res.json(docs);
   });
 });
